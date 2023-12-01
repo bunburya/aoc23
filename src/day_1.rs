9e1ce7b400e58b784a4a6a94723526514cf69667
bunburya <dev@bunburya.eu>
2023-12-01 17:46:50 +0000

    initial commit incl day 1

---
+++ src/day_1.rs
@@ -0,0 +1,81 @@
+use std::collections::HashMap;
+
+fn new_digit_word_map<'a>() -> HashMap<&'a str, char> {
+    let mut digits = HashMap::new();
+    digits.insert("one", '1');
+    digits.insert("two", '2');
+    digits.insert("three",'3');
+    digits.insert("four", '4');
+    digits.insert("five", '5');
+    digits.insert("six", '6');
+    digits.insert("seven", '7');
+    digits.insert("eight", '8');
+    digits.insert("nine", '9');
+    digits
+}
+
+fn words_to_digits(s: &String) -> String {
+    let chars: Vec<char> = s.chars().collect();
+    let digit_word_map = new_digit_word_map();
+    let mut new_s = String::new();
+    let mut next_push_unmatched = 0;  // the next position at (or after) which we will push a
+                                      // character, which doesn't match a "number word", to the new
+                                      // string.
+    let mut word_size: usize;  // the size of a number word
+    let mut matched: bool;  // whether a number word match has been found
+    let mut to_test: String;  // the slice of text we are to test against each number word
+    for (i, c) in chars.iter().enumerate() {
+        matched = false;
+        for (w, d) in digit_word_map.iter() {
+            // for each number word, get the length of that word, look ahead that many
+            // characters and compare to the number word
+            word_size = w.len();
+            if i + word_size > chars.len() {
+                // looking ahead would take us out of bounds (so word can't match)
+                continue;
+            }
+            to_test = chars[i..i+word_size].iter().collect();
+            if to_test == *w {
+                // found a match
+                new_s.push(*d);
+                matched = true;
+                next_push_unmatched = i + word_size;
+                break;
+            }
+        }
+        if (!matched) && (i >= next_push_unmatched) {
+            // didn't match any number word, so just push the character directly
+            new_s.push(*c);
+        }
+    }
+    new_s
+}
+
+fn first_digit<I>(chars: I) -> char where I: Iterator<Item = char> {
+    // Find the first numeric character in a sequence of characters
+    for c in chars {
+        if c.is_numeric() {
+            return c
+        }
+    }
+    panic!("No digits found.")
+}
+
+pub fn part_1(s: &String) -> String {
+    let mut cal: Vec<u32> = vec!();
+    for line in s.lines() {
+        let chars = line.chars();
+        let first = first_digit(chars.clone()).to_digit(10)
+            .expect("First digit character cannot be converted to number.");
+        let last = first_digit(chars.rev()).to_digit(10)
+            .expect("Last digit character cannot be converted to number.");
+        cal.push((first * 10) + last)
+    }
+    let total: u32 = cal.iter().sum();
+    total.to_string()
+}
+
+pub fn part_2(s: &String) -> String {
+    let fixed = words_to_digits(s);
+    part_1(&fixed)
+}
---
+++ src/main.rs
@@ -0,0 +1,61 @@
+mod day_1;
+
+use std::{fs, path};
+use std::env;
+use std::time::Instant;
+
+fn main() {
+
+    let funcs: [[Option<fn(&String) -> String>; 2]; 25] = [
+        [Some(day_1::part_1), Some(day_1::part_2)],
+        [None, None],
+        [None, None],
+        [None, None],
+        [None, None],
+        [None, None],
+        [None, None],
+        [None, None],
+        [None, None],
+        [None, None],
+        [None, None],
+        [None, None],
+        [None, None],
+        [None, None],
+        [None, None],
+        [None, None],
+        [None, None],
+        [None, None],
+        [None, None],
+        [None, None],
+        [None, None],
+        [None, None],
+        [None, None],
+        [None, None],
+        [None, None]
+    ];
+
+    let day = env::args().nth(1).expect("Must specify a day.")
+        .parse::<usize>().expect("Day must be integer.");
+    let part = env::args().nth(2).expect("Must specify a part.")
+        .parse::<usize>().expect("Part must be an integer.");
+    let input_fpath_str = env::args().nth(3)
+        .expect("Must specify a path to input data.");
+    let input_fpath = path::Path::new(&input_fpath_str);
+    let func = &funcs[day-1][part-1];
+
+    match func {
+        Some(f) => {
+            let start_time = Instant::now();
+            let input = fs::read_to_string(input_fpath)
+                .expect("Could not read input file.");
+            let output = f(&input);
+            let end_time = Instant::now();
+            let duration = end_time - start_time;
+            println!("Day {}, part {} answer:", day, part);
+            println!("{}", output);
+            println!("Time taken: {:#?}", duration)
+        },
+        None => panic!("No function found for day {}, part {}.", day, part)
+    }
+}
+
